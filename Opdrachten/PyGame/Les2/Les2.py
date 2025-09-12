0b7c0c4c00073e76d58c2e48a174ccf2f6677dc3
TylerBrandt0 <123280@chrlyceumdelft.nl>
2025-09-12 07:52:56 +0000

    ert

--- Opdrachten/PyGame/Les2/Les2.py
+++ Opdrachten/PyGame/Les2/Les2.py
@@ -30,9 +30,21 @@ clock = pygame.time.Clock()
 running = True
 test_font = pygame.font.Font("/workspaces/pygame-oefeningen-TylerBrandt0/Opdrachten/PyGame/Les2/fonts/Soccer.ttf", 50)
 
+surface = pygame.Surface((800, 400))
+surface.fill("blue")
+grass = pygame.Surface((800, 100))
+grass.fill("dark green")
 voetbal_surface = pygame.image.load("Opdrachten/PyGame/Les2/graphics/voetbal.png")
-speler_surface = pygame.image.load("/workspaces/pygame-oefeningen-TylerBrandt0/Opdrachten/PyGame/Les2/graphics/speler.png")
+speler = pygame.image.load("/workspaces/pygame-oefeningen-TylerBrandt0/Opdrachten/PyGame/Les2/graphics/speler.png")
 tekst_surface = test_font.render("Score", False, "white")
+tekst_surface2 = test_font.render("0 - 0", False, "dark red")
+goal = pygame.image.load("/workspaces/pygame-oefeningen-TylerBrandt0/Opdrachten/PyGame/Les2/graphics/goal.svg")
+
+speler = pygame.transform.scale(speler, (150, 150))
+speler2 = pygame.transform.flip(speler, True, False)
+voetbal_surface = pygame.transform.scale(voetbal_surface, (35, 35))
+goal = pygame.transform.scale(goal, (200, 200))
+goal2 = pygame.transform.flip(goal, True, False)
 
 while running:
 
@@ -40,9 +52,15 @@ while running:
     if event.type == pygame.QUIT:
       running = False
 
-  screen.blit(tekst_surface, (200, 100))
-  screen.blit(voetbal_surface, (200, 200))
-  screen.blit(speler_surface, (200, 200))
+  screen.blit(surface, (0, 0))
+  screen.blit(grass, (0, 300))
+  screen.blit(goal, (700, 150))
+  screen.blit(goal2, (-100, 150))
+  screen.blit(tekst_surface, (0, 0))
+  screen.blit(tekst_surface2, (35, 50))
+  screen.blit(voetbal_surface, (365, 300))
+  screen.blit(speler, (550, 200))
+  screen.blit(speler2, (100, 200))
 
   pygame.display.update()
   clock.tick(60)
